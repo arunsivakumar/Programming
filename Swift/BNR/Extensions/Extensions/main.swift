880cb2c2315a24a5a9c5bef4905ffd3e80729f04
Arun Sivakumar <arunsivakumar@live.com>
2020-03-01 16:52:22 +0800

    migrate extensions to swift 5

--- Swift/BNR/Extensions/Extensions/main.swift
+++ Swift/BNR/Extensions/Extensions/main.swift
@@ -8,31 +8,27 @@
 
 import Foundation
 
-print("Hello, World!")
-
 typealias Velocity = Double
-
-
-extension Velocity{
+extension Velocity {
     var kph: Velocity { return self * 1.609 }
     var mph : Velocity { return self }
     
 }
 
-protocol Vehicle{
+protocol Vehicle {
     var topSpeed: Velocity { get }
     var numberOfDoors : Int { get }
     var hasFlatbed : Bool { get }
 }
 
-struct Car{
+struct Car {
     let make: String
     let model: String
     let year: Int
     let color: String
     let nickname: String
-    var gasLevel: Double{
-        willSet{
+    var gasLevel: Double {
+        willSet {
             precondition(newValue <= 1.0 && newValue >= 0.0, "New value must be between 0 and 1")
         }
     }
@@ -40,7 +36,7 @@ struct Car{
 
 //protocol to extension
 
-extension Car: Vehicle{
+extension Car: Vehicle {
     var topSpeed: Velocity { return 180 }
     var numberOfDoors: Int { return 4 }
     var hasFlatbed: Bool { return false }
@@ -48,7 +44,7 @@ extension Car: Vehicle{
 }
 // init for extension
 
-extension Car{
+extension Car {
     init(make: String, model: String, year: Int) {
         self.init(make: make,
                   model: model,
@@ -61,13 +57,13 @@ extension Car{
 
 // nested types
 
-extension Car{
-    enum Kind{
+extension Car {
+    enum Kind {
         case coupe, sedan
     }
     
-    var kind: Kind{
-        if numberOfDoors == 2{
+    var kind: Kind {
+        if numberOfDoors == 2 {
             return .coupe
         } else {
             return .sedan
@@ -79,13 +75,13 @@ print(c.kind)
 
 // extension with functions
 
-extension Car{
-    mutating func emptyGas(by amount: Double){
+extension Car {
+    mutating func emptyGas(by amount: Double) {
         precondition(amount <= 1 && amount > 0, "Amount to be removed must be between 0 and 1")
         gasLevel -= amount
     }
     
-    mutating func fillGas(){
+    mutating func fillGas() {
         gasLevel = 1.0
     }
 }
@@ -93,9 +89,6 @@ print(c.emptyGas(by: 0.3))
 print(c.gasLevel)
 print(c.fillGas())
 print(c.gasLevel)
-
-
-
 
 
 
